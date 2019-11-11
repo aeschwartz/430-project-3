c2dfd6314f1caeae8a10724d66469e40301c45da
Andrew Schwartz <andrewschwartz99@gmail.com>
2019-11-11 03:27:25 -0500

    basic prototype functionality

--- server/app.js
+++ server/app.js
@@ -1,13 +1,15 @@
 const path = require('path');
 const express = require('express');
-const url = require('url');
+// const url = require('url');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
-app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted`)));
+const app = express();
+
+app.use('/', express.static(path.resolve(`${__dirname}/../hosted`)));
 
 app.listen(port, (err) => {
-    if (err) throw err;
-    console.log(`Listening on port ${port}`);
+  if (err) throw err;
+  console.log(`Listening on port ${port}`);
 });
 
